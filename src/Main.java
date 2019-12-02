639d2ceddc5c98b7cacac4f6f11b6b53ad0907e4
MikhailTokhomirov <Mikhail_Tikhomirov@epam.com>
2019-12-02 19:30:46 +0400

    комит хело ворд

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello WORLD!"); //есть 2  направления потока "из" и "в" out=из в итоге команда
+        //системы из показать(вывести/печать)
+    }
+}
+
+    //public static void main(String[] args) =psvm
